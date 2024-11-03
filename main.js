cc090210279de0601ef12e81e5ee49258c8c082b
Wajid Daud Tamboli <143938720+wajiddaudtamboli@users.noreply.github.com>
2024-11-03 20:45:34 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+var typed= new Typed(".text" , {
+    strings: ["Frontend Developer" , "Web Developer" , "Future Software Engineer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
+
+
